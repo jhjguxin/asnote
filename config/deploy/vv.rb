2a860d7fbc748685bffdff68510ac92faf60e969
Francis Jiang <864248765@qq.com>
2012-10-24 15:55:22 +0800

    add rvm setting

--- config/deploy/vv.rb
+++ config/deploy/vv.rb
@@ -1,2 +1,3 @@
-set :repository,  "git@192.168.1.100:andersen/younoter.git"
+#set :repository,  "git@192.168.1.100:andersen/younoter.git"
+set :repository,  "git@github.com:jhjguxin/asnote.git"
 server "192.168.1.100", :app, :web, :db, :primary => true
